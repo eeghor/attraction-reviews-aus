233913b3833e3208bc9ea4eecda7f7bde4284551
Unknown <eeghor@gmail.com>
2019-02-18 17:14:29 +1100

    extra elements

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-
-
 df = pd.read_csv('data/brisb.csv', parse_dates=['date_of_experience'], infer_datetime_format=True)
 
 def selector(dk):
@@ -38,7 +36,10 @@ def selector(dk):
         print('empty result!')
         raise Exception()
 
-genders = {'m', 'f'}
+genders = ['m', 'f']
+
+age_groups = [ag for ag in set(df['age']) if ag] 
+
 trav_types = ['foodie', 'luxury traveller', 'beach goer']
 
 colors = ['orange', '#2C72EC', '#24C173', '#E332E3', '#E39132', '#948778']
@@ -49,18 +50,22 @@ for g in genders:
 
 	for t in trav_types:
 
-		d = selector({'gender': g, t: 1})
+		d = selector({'gender': g, t: 'yes'})
 
 		d1 = d[['date_of_experience', 'id']].groupby(['date_of_experience']).count().reset_index()
 
 		d1_scatter = go.Scatter(x=d1.date_of_experience, 
-                            y=d1.id, mode='markers', 
-                               marker=dict(size=12, line=dict(width=0), color=colors.pop()),
+                            	y=d1.id, mode='markers', 
+                               	marker=dict(size=12, line=dict(width=0), color=colors.pop()),
                                 name=t, text='pigeons diamonds ticket cruise champaign',)
 		data.append(d1_scatter)
 
 
-app.layout = html.Div(children=[ html.H2(children='Brisbane Attractions', style={'textAlign': 'center'}),
+app.layout = html.Div(children=[
+
+						html.Div(children=
+									[html.H2(children='Brisbane Attractions', style={'textAlign': 'center'}),
+										dcc.Markdown("""compare **opinions** by tourist segment""")]),
 
     					dcc.Graph(
     					    id='brisb-reviews',
@@ -72,7 +77,30 @@ app.layout = html.Div(children=[ html.H2(children='Brisbane Attractions', style=
     					            margin={'l': 40, 'b': 80, 't': 10, 'r': 10},
     					            legend={'x': 0, 'y': 1},
     					            hovermode='closest'
-    					        )})])
+    					        )}),
+
+    					html.Div(children=[
+						html.Span(children=[
+
+    							html.Label('age group'),
+
+    							dcc.Dropdown(
+    					    		options=[{'label': ag, 'value': ag} for ag in age_groups],
+       					 			value=age_groups[0],
+       					 			multi=False)
+    									]),
+
+						html.Span(children=[
+
+    							html.Label('gender'),
+
+    							dcc.Dropdown(
+    					    		options=[{'label': g, 'value': g} for g in genders],
+       					 			value=genders[0])
+    									])
+						])
+						])
+
 
 if __name__ == '__main__':
 
