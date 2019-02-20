8b74e408a924048d882256b8d221581b528a9963
eeghor <eeghor@gmail.com>
2019-02-20 22:58:05 +1100

    heroku commit

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import dash_bootstrap_components as dbc
 
 import pandas as pd
 
-import random
-
 import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -20,6 +18,7 @@ df = pd.read_csv('data/brisb.csv', parse_dates=['date_of_experience'],
 genders = ['m', 'f']
 age_groups = sorted([ag for ag in set(df['age']) if '-' in str(ag)], key=lambda x: int(x.split('-')[0]))
 tourist_types = [c for c in df.columns if {'yes', 'no'} <= set(df[c])]
+countries = ['Australia', 'UK']
 
 colors = {'seg1': 'orange',
 			'seg2': '#2C72EC'}
@@ -67,6 +66,17 @@ def make_number_reviews_scatter(df, name, color):
 
 	return sc
 
+def make_dropdown(attr_name, seg_num, attr_options):
+
+	return dbc.DropdownMenu(
+                    id=f'ddm-{attr_name}-seg-{seg_num}',
+                    label=attr_name.title(),
+                    bs_size="sm",
+                    nav=True,
+                    in_navbar=True,
+                    children=[dbc.DropdownMenuItem(_, id=f'seg-{seg_num}-' + _, disabled=False) for _ in attr_options],
+                    )
+
 
 navbar = dbc.NavbarSimple(
 	brand="Demo TripAdvisor Dashboard",
@@ -91,30 +101,13 @@ body = dbc.Container([
                 dbc.CardHeader(dbc.Badge("Segment 1", color='info')),
                 dbc.CardBody(
                     [
-                    dbc.Row(
-                    [dbc.Col(dbc.DropdownMenu(
-                    						id='ddm-age-seg-1',
-                        					label="Age",
-                        					bs_size="sm",
-                        					children=[dbc.DropdownMenuItem(ag, id='seg-1-' + ag, disabled=False) for ag in age_groups],
-                    						)),
-                    
-                    dbc.Col(dbc.DropdownMenu(
-                    				id='ddm-gen-seg-1',
-                    				label="Gender",
-                    				bs_size="sm",
-                    				style={'color': 'info'},
-                    				in_navbar=True,
-                    				children=[dbc.DropdownMenuItem('m', id='gender1-m', disabled=False),
-                    								dbc.DropdownMenuItem('f', id='gender1-f', disabled=False)],
-                    			)),
-                    dbc.Col(dbc.DropdownMenu(
-                    					id='ddm_ttype1',
-                        				label="Type",
-                        				bs_size="sm",
-                        				children=[dbc.DropdownMenuItem(tt, id='-'.join(['ttype1', str(tt)])) for tt in tourist_types],
-                    					)),]
-                    )
+						dbc.Nav(
+                    	[
+                    	make_dropdown('age', '1', age_groups), 
+                    	make_dropdown('gender', '1', genders),
+                 		make_dropdown('type', '1', tourist_types),
+                 		make_dropdown('country', '1', countries),
+                    	])
 
                     ]
                 ),
@@ -126,27 +119,13 @@ body = dbc.Container([
                 dbc.CardHeader(dbc.Badge("Segment 2", color='info')),
                 dbc.CardBody(
                     [
-                    dbc.Row(
-                    [dbc.Col(dbc.DropdownMenu(
-                    							id='ddm_age2',
-                        						label="Age",
-                        						bs_size="sm",
-                        						children=[dbc.DropdownMenuItem(ag, id='-'.join(['ag2', str(ag)])) for ag in age_groups],
-                    						 ), width='auto'),
-                    
-                             dbc.Col(dbc.DropdownMenu(
-                    							id='ddm_gender2',
-                        						label="Gender",
-                        						bs_size="sm",
-                        						children=[dbc.DropdownMenuItem(g, id='-'.join(['gender2', str(g)])) for g in genders],
-                    							)),
-                             dbc.Col(dbc.DropdownMenu(
-                    							id='ddm_ttype2',
-                        						label="Type",
-                        						bs_size="sm",
-                        						children=[dbc.DropdownMenuItem(tt, id='-'.join(['ttype2', str(tt)])) for tt in tourist_types],
-                    									)), ]
-                    )
+                    dbc.Nav(
+                    	[
+                    	make_dropdown('age', '2', age_groups), 
+                    	make_dropdown('gender', '2', genders),
+                 		make_dropdown('type', '2', tourist_types),
+                 		make_dropdown('country', '2', countries),
+                    	])
                     ]
                 ),
             ]
@@ -167,47 +146,53 @@ app.layout = html.Div([navbar, body])
 @app.callback(
     dash.dependencies.Output('brisb-reviews', 'figure'), # will be updating the figure part of the Graph
     [dash.dependencies.Input('seg-1-' + ag, 'n_clicks_timestamp') for ag in age_groups] +
-    	[dash.dependencies.Input('ag2-' + ag, 'n_clicks_timestamp') for ag in age_groups] +
-    		[dash.dependencies.Input('gender1-m', 'n_clicks_timestamp'),
-    			dash.dependencies.Input('gender1-f', 'n_clicks_timestamp')]
+    	[dash.dependencies.Input('seg-2-' + ag, 'n_clicks_timestamp') for ag in age_groups] +
+    		[dash.dependencies.Input('seg-1-' + g, 'n_clicks_timestamp') for g in genders] +
+    			[dash.dependencies.Input('seg-2-' + g, 'n_clicks_timestamp') for g in genders]
     	)  # what inputs need to be monitored to update the output (figure in Graph)?
 
 def update_graph(*menu_items_click_timestamps):
 
-	print('menu_items_click_timestamps=', menu_items_click_timestamps)
 	t = list(menu_items_click_timestamps)
 
-	print('menu_items_click_timestamps=', t)
-	tss_seg1 = [_ if _ else 0 for _ in t[:len(age_groups)]]
-	print('tss_seg1=', tss_seg1)
-	tss_seg2 = [_ if _ else 0 for _ in t[len(age_groups):2*len(age_groups)]]
-	print('tss_seg2=', tss_seg2)
-	tss_gend_seg1 = t[2*len(age_groups):]
-	print('tss_gend_seg1=', tss_gend_seg1)
+	tts_seg1_age = [_ if _ else 0 for _ in t[:len(age_groups)]]
+	tts_seg2_age = [_ if _ else 0 for _ in t[len(age_groups):2*len(age_groups)]]
 
-	if not any(tss_seg1):
+	tts_seg1_gend = t[2*len(age_groups):2*len(age_groups)+len(genders)]
+	tts_seg2_gend = t[2*len(age_groups)+len(genders):2*len(age_groups)+2*len(genders)]
+
+	if not any(tts_seg1_age):
 		selected_ag_seg1 = age_groups[-1]
-		print('default segment 1: ', selected_ag_seg1)
 	else:
-		max_ts_seg1 = max([ts if ts else 0 for ts in tss_seg1])
-		print('max_ts_seg1=', max_ts_seg1)
-		selected_ag_seg1 = age_groups[tss_seg1.index(max_ts_seg1)]
-		print('selected_ag_seg1=', selected_ag_seg1)
+		max_ts_seg1 = max([ts if ts else 0 for ts in tts_seg1_age])
+		selected_ag_seg1 = age_groups[tts_seg1_age.index(max_ts_seg1)]
 
-	if not any(tss_seg2):
+	if not any(tts_seg2_age):
 		selected_ag_seg2 = age_groups[-2]
-		print('default segment 2: ', selected_ag_seg2)
 	else:
-		max_ts_seg2 = max([ts if ts else 0 for ts in tss_seg2])
-		selected_ag_seg2 = age_groups[tss_seg2.index(max_ts_seg2)]
-		print('selected_ag_seg2=', selected_ag_seg2)
+		max_ts_seg2 = max([ts if ts else 0 for ts in tts_seg2_age])
+		selected_ag_seg2 = age_groups[tts_seg2_age.index(max_ts_seg2)]
+
+	if not any(tts_seg1_gend):
+		selected_gen_seg1 = genders[-1]
+	else:
+		max_gen_seg1 = max([ts if ts else 0 for ts in tts_seg1_gend])
+		selected_gen_seg1 = genders[tts_seg1_gend.index(max_gen_seg1)]
+
+	if not any(tts_seg2_gend):
+		selected_gen_seg2 = genders[-1]
+	else:
+		max_gen_seg2 = max([ts if ts else 0 for ts in tts_seg2_gend])
+		selected_gen_seg2 = genders[tts_seg2_gend.index(max_gen_seg2)]
 
 
-	df_seg1 = selector({'age': selected_ag_seg1})
-	df_seg2 = selector({'age': selected_ag_seg2})
+	df_seg1 = selector({'age': selected_ag_seg1, 'gender': selected_gen_seg1})
+	df_seg2 = selector({'age': selected_ag_seg2, 'gender': selected_gen_seg2})
 
-	fig_data = [make_number_reviews_scatter(df_seg1, name='segment 1: ' + selected_ag_seg1, color=colors['seg1']), 
-				make_number_reviews_scatter(df_seg2, name='segment 2: ' + selected_ag_seg2, color=colors['seg2'])]
+	fig_data = [make_number_reviews_scatter(df_seg1, name='segment 1: ' + '/'.join([selected_gen_seg1, selected_ag_seg1]), 
+						color=colors['seg1']), 
+				make_number_reviews_scatter(df_seg2, name='segment 2: ' + '/'.join([selected_gen_seg2, selected_ag_seg2]), 
+						color=colors['seg2'])]
 
 	return {'data': fig_data,
 			'layout': go.Layout(
@@ -215,8 +200,9 @@ def update_graph(*menu_items_click_timestamps):
 							yaxis={'title': 'Number of Reviews'},
 							margin={'l': 40, 'b': 80, 't': 10, 'r': 10},
 							legend={'x': 0, 'y': 1},
-							height=400,
-							hovermode='closest')}
+							height=500,
+							hovermode='closest')
+			}
 
 if __name__ == '__main__':
 	app.run_server(debug=True)
